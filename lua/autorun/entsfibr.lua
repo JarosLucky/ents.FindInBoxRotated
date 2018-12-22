e38862b941009c24571bf62fdd4a42d954389365
JarosLucky <snyashka@gmail.com>
2018-12-22 18:50:28 +0700

    now worked and optimized

--- lua/autorun/entsfibr.lua
+++ lua/autorun/entsfibr.lua
@@ -1,7 +1,7 @@
 function ents.FindInBoxRotated(pos,ang,x,y,z,dist)
   local tbl={}
-  for _,v in pairs(ents.FindInSphere(Vector(),dist))do
-    local vec=WorldToLocal(v:GetPos(),ang,pos,Angle())
+  for _,v in ipairs(ents.FindInSphere(pos,dist))do
+    local vec=WorldToLocal(v:GetPos(),v:GetAngles(),pos,ang)
     if(vec.x<=x&&vec.x<=-x)&&(vec.y<=y&&vec.y<=-y)&&(vec.z<=z&&vec.z<=-z)then
       table.insert(tbl,v)
     end
