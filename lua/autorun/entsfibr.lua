90f11520bcae02342dd12244624f860f7a2fbc08
JarosLucky <19414192+JarosLucky@users.noreply.github.com>
2025-01-17 01:36:03 +0700

    Localize all metatables

--- lua/autorun/entsfibr.lua
+++ lua/autorun/entsfibr.lua
@@ -2,12 +2,16 @@ local ipairs = ipairs
 local WorldToLocal = WorldToLocal
 local table_insert = table.insert
 local ents_FindInSphere = ents.FindInSphere
+local ENTITY = FindMetaTable("Entity")
+local WithinAABox = FindMetaTable("Vector").WithinAABox
+local GetPos = ENTITY.GetPos
+local GetAngles = ENTITY.GetAngles
 
 function ents.FindInBoxRotated(vCenter, aRotation, vMinBox, vMaxBox, flDist)
 	local tResult = {}
 
-	for _, eTarget in ipairs(ents_FindInSphere(vCenter, flDist))do
-		if WorldToLocal(eTarget:GetPos(), eTarget:GetAngles(), vCenter, aRotation):WithinAABox(vMinBox, vMaxBox) then
+	for _, eTarget in ipairs(ents_FindInSphere(vCenter, flDist)) do
+		if WithinAABox(WorldToLocal(GetPos(eTarget), GetAngles(eTarget), vCenter, aRotation), vMinBox, vMaxBox) then
 			table_insert(tResult, eTarget)
 		end
 	end
